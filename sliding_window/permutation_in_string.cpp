b1daa9473a3712647873bdde7ac419b9d3e24a89
lowLevel02-Dev <uthkarsh75@gmail.com>
2026-07-06 10:55:20 +0530

    567.permutation_in_string changes

--- sliding_window/permutation_in_string.cpp
+++ sliding_window/permutation_in_string.cpp
@@ -1,41 +1,55 @@
-#include<iostream>
-#include<unordered_map>
-#include<unordered_set>
-#include<vector>
-#include<string>
-using namespace std; 
+#include <iostream>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include <string>
+using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    bool checkInclusion(string s1, string s2) {
-        vector<int>freq1(26,0);
-        vector<int>freq2(26,0);
-        int left = 0;
-        for(char c : s1){
+    bool checkInclusion(string s1, string s2)
+    {
+        if (s1.size() > s2.size())
+            return false;
+
+        vector<int> freq1(26, 0);
+        vector<int> freq2(26, 0);
+
+        for (char c : s1)
             freq1[c - 'a']++;
-        }    
+
         for (int i = 0; i < s1.size(); i++)
             freq2[s2[i] - 'a']++;
-        if(freq1 == freq2){
+
+        if (freq1 == freq2)
             return true;
-        }
-        for(int right = s1.size(); right < s2.size(); right++){
+
+        int left = 0;
+
+        for (int right = s1.size(); right < s2.size(); right++)
+        {
+
             freq2[s2[right] - 'a']++;
+
             freq2[s2[left] - 'a']--;
+
             left++;
-            if(freq1 == freq2){
+
+            if (freq1 == freq2)
                 return true;
-            }
         }
+
         return false;
     }
 };
 
-int main(){
-    string s1 = "ab";   
+int main()
+{
+    string s1 = "ab";
     string s2 = "eidbaooo";
     Solution sol;
-    bool res = sol.checkInclusion(s1,s2);
-    cout<<res;
+    bool res = sol.checkInclusion(s1, s2);
+    cout << res;
     return 0;
 }
